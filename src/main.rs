3e2a4b957bd56f7e923d67d4092a33a16801bf16
Fangkang <fangkangmi@outlook.com>
2024-11-08 16:21:16 +0000

    Enhance Guess Number game with improved output and comparison logic

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@
 // 1. println! macro
 // 2. String type
 
+//trait is a way to define shared behavior in an abstract way.
+
 use std::io;  // prelude
+use std::cmp::Ordering; 
 use rand::Rng; // trait
 
 fn main() {
     println!("Guess number game!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    println!("The secret number is: {}", secret_number);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..101);
+    // ^^^^^^^^^^^^^^
+    // rand::thread_rng() returns a random number generator instance
+    // .gen_range(1..101) is a method call on that instance
 
     // let is used to create a variable
     // mut is used to make the variable mutable
@@ -23,5 +27,18 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line"  );
 
+    // Rust allows shadowing
+    // Shadowing means that you can declare a new variable with the same name as a previous variable
+    // u32 is an unsigned 32-bit integer. Range: 0 to 4,294,967,295.
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    // => is used to return a value
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
